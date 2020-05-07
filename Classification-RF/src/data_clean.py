5d25b5e687a05bfa4dd084ba93bcb81031fd54c5
Anjana0987 <anjana.rajasekhar1228@gmail.com>
2020-05-07 15:07:27 -0500

    training models

--- Classification-RF/src/data_clean.py
+++ Classification-RF/src/data_clean.py
@@ -10,12 +10,31 @@ class CleaningData:
         df['Title'] = df.Name.str.extract(r',\s*([^\.]*)\s*\.', expand=False)
         # Converting age to age groups
         bins = [0, 5, 15, 25, 35, 45, 55, 65, 80, np.inf]
-        #names = ['<2', '5-15', '15-25', '25-35', '35-45', '45-55', '55-65', '65-80', '80+']
+        # Range: ['<2', '5-15', '15-25', '25-35', '35-45', '45-55', '55-65', '65-80', '80+']
         names = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         df['AgeRange'] = pd.cut(df['Age'], bins, labels=names)
-        print(df['Age'], df['AgeRange'])
+        bin = [0, 10, 30, 50, 70, 90, 110, 130, np.inf]
+        name = ['1', '2', '3', '4', '5', '6', '7', '8']
+        df['FareRange'] = pd.cut(df['Fare'], bin, labels=name)
+        # Replacing Categorical data to numerical categories: Sex Column
+        df.Sex.replace(['male', 'female'], [1, 0], inplace=True)
+        # Replacing Categorial data to numerical categories: Embark Column
+        df.Embarked.replace(['S', 'C', 'Q'], [0, 1, 2], inplace=True)
+        # Replacing Categorical data to numerical categories: Titke Column
+        df.Title.replace(['Mr','Mrs', 'Miss', 'Master', 'Don', 'Rev', 'Dr', 'Mme', 'Ms', 'Major', 'Lady', 'Sir', 'Mlle', 'Col', 'Capt', 'the Countess', 'Jonkheer'], [1, 2, 3, 4,5,6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], inplace = True)
         return df
 
     def drop_columns(self, df):
-
+        # Drop the PassengerID column
+        del df['PassengerId']
+        # Drop the Name column
+        del df['Name']
+        # Drop the Age column
+        del df['Age']
+        # Drop the ticked column
+        del df['Ticket']
+        # Drop the Fare column
+        del df['Fare']
+        # Drop the Cabin column
+        del df['Cabin']
         return df
--- Classification-RF/src/main.py
+++ Classification-RF/src/main.py
@@ -16,11 +16,19 @@ cd = CleaningData()
 train_df = dw.read_csv_in_dataframe(train_data_filepath)
 test_df = dw.read_csv_in_dataframe(test_data_filepath)
 
-# Replace Missing values
+#Data Wrangling
+train_data_study = dw.data_wrangle(train_df)
+
+# Replace Missing values and Cleaning columns
 train_df = cd.cleaning_columns(train_df)
 test_df = cd.cleaning_columns(test_df)
 
-#Data Wrangling
-train_data_study = dw.data_wrangle(train_df)
+# Drop columns
+train_df = cd.drop_columns(train_df)
+test_df = cd.drop_columns(test_df)
+
+# Train the models
+
+
 
 
